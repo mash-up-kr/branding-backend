1b27f9958d5ded5e5f791467258334ad8d6b56ba
csh <csh@comcom.ai>
2020-06-20 16:30:06 +0900

    Init express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('HELLo world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT}`);
+});
