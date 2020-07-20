9684ed9bd8a486c9c72ea1ca0db93f2dffb21de1
Sanghee Choi <32831939+pengin7384@users.noreply.github.com>
2020-07-21 02:30:35 +0900

    Feature/connect spread sheet (#4)
    * Ignore config directory
    
    * Add google-spreadsheet dependency
    
    * Add getHeaderListFromId
    
    * Update package-lock.json
    
    * Add getHeaderListFromId sample

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { sequelize } from './models';
+import {sequelize} from './models';
+import {getHeaderListFromId} from './util/spread_sheet';
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('HELLo world');
+});
+
+app.get('/sheet', async (req, res) => {
+  const testSheetId = '122TOSC-YycmW3uhK5MlwdVawSKdjVURHRwDKJZbG0kE';
+  res.send(await getHeaderListFromId(testSheetId));
 });
 
 app.listen(PORT, () => {
