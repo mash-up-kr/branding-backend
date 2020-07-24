6179994cb0e3b71ac76e6fe83c23510c259cfa92
korea8378 <korea8378@naver.com>
2020-07-24 23:11:35 +0900

    [ADD] 다수(지원자들)의 지원상태 변경 API

--- api/routes/applicant.js
+++ api/routes/applicant.js
@@ -1,7 +1,25 @@
 
 const express = require('express');
+const applicantService = require('../../service/applicant');
 
 const router = express.Router();
 
-  
+router.route('/list')
+  .patch(async (req, res, next) => {
+    const {applicants_ids, application_status} = req.body;
+    const {role: role} = req.decoded;
+    try {
+      const result = await applicantService.changeListStatus(role, applicants_ids, application_status);
+      res.status('200')
+      .json({
+        success: true,
+        message: { applicant : result }
+      });
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+
+
 module.exports = router;
---
+++ service/applicant.js
@@ -0,0 +1,42 @@
+const ROLE = require("../models/role");
+const Applicant = require('../models/applicant');
+const db = require('../models/index');
+
+async function changeListStatus(role, applicantIds, applicantionStatus) {
+  if(!role == ROLE.ADMIN) {
+    const error = new Error('No Atuthentification');
+    error.status = 403;
+    throw error;
+  }
+
+
+  const t = await db.sequelize.transaction();
+  
+  const applicants = await Applicant.findAll({
+    where: {
+      id: {
+        [db.Sequelize.Op.in]: applicantIds
+      }
+    }
+  });
+
+  try {
+    for(let i = 0; i < applicants.length; i++) {
+     await applicants[i].update(
+       { application_status:applicantionStatus, update_time: Date.now() }, 
+       { transaction: t });
+    }
+    
+    await t.commit();
+  } catch (error) {
+    await t.rollback();
+  }
+
+  return {
+    applicants : applicants
+  }
+}
+
+export {
+  changeListStatus
+}
