6e2b11f2dadd2c08974dd58382f6fc747e8c8a79
Sanghee Choi <32831939+pengin7384@users.noreply.github.com>
2020-07-26 06:55:09 +0900

    Allow cors (#30)
    Co-authored-by: csh <csh@comcom.ai>

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ const PORT = 3001;
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
+  next();
+});
+
 app.use('/v1/', authRoute);
 app.use('/v1/users', userRoute);
 app.use('/v1/applicant-status', applicantStatusRoute);
