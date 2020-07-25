ff2349d6b60337acdab46e4f7e7e3bef19d9e191
csh <csh@comcom.ai>
2020-07-26 02:31:15 +0900

    Add getApplicantFromId

--- util/mocker.js
+++ util/mocker.js
@@ -90,6 +90,72 @@ function createGetApplicantsMock() {
   return mock;
 }
 
+function getApplicantFromId(id) {
+  const data = {
+    1: {
+      'id': 1,
+      'team': {
+        'id': 2,
+        'name': 'Web',
+      },
+      'name': '김주성',
+      'email': 'jusungbang@mashup.com',
+      'phone': '010-1234-5678',
+      'timestamp': 1595686254,
+      'status': 'APPLICATION_COMPLETION',
+    },
+    2: {
+      'id': 2,
+      'team': {
+        'id': 4,
+        'name': 'Design',
+      },
+      'name': '남궁욱',
+      'email': 'kkkoungg@mashup.com',
+      'phone': '010-1234-5678',
+      'timestamp': 1595686254,
+      'status': 'DOCUMENT_PASS',
+    },
+    3: {
+      'id': 3,
+      'team': {
+        'id': 4,
+        'name': 'Design',
+      },
+      'name': '조성윤',
+      'email': 'sung@mashup.com',
+      'phone': '010-1234-5678',
+      'timestamp': 1595686254,
+      'status': 'DOCUMENT_PASS',
+    },
+    4: {
+      'id': 4,
+      'team': {
+        'id': 4,
+        'name': 'Design',
+      },
+      'name': '김형진',
+      'email': 'hj@mashup.com',
+      'phone': '010-1234-5678',
+      'timestamp': 1595686254,
+      'status': 'DOCUMENT_PASS',
+    },
+    5: {
+      'id': 5,
+      'team': {
+        'id': 1,
+        'name': 'Android',
+      },
+      'name': '현멍즌',
+      'email': 'mj@mashup.com',
+      'phone': '010-1234-5678',
+      'timestamp': 1595686254,
+      'status': 'DOCUMENT_PASS',
+    },
+  };
+}
+
 export {
   createGetApplicantsMock,
+  getApplicantFromId,
 };
