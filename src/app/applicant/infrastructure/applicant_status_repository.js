cb5745cb9aaae6ec6e9fe56542d67af3f4c470d1
jo-bata <jobata@skuniv.ac.kr>
2020-08-17 03:29:14 +0900

    Refactor repository

--- src/app/applicant/infrastructure/applicant_status_repository.js
+++ src/app/applicant/infrastructure/applicant_status_repository.js
@@ -1,76 +1,67 @@
 const db = require('../../../common/model/sequelize.js');
 
 const findAllApplicants = async recruitingId => {
-  const query = `SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time
-                  FROM teams as t JOIN applicants as a 
-                      ON t.id = a.teams_id
-                  WHERE t.recruiting_id = :recruitingId`;
+  const query = 'SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time ' +
+    'FROM teams as t ' +
+    'JOIN applicants as a ON t.id = a.teams_id ' +
+    'WHERE t.recruiting_id = :recruitingId';
+  const options = {
+    replacements: {recruitingId},
+    type: db.Sequelize.QueryTypes.SELECT,
+    raw: true,
+  };
+  const result = await db.sequelize.query(query, options);
   
-  const results = await db.sequelize.query(
-    query, 
-    {
-    replacements: {recruitingId: recruitingId}, 
-    type: db.Sequelize.QueryTypes.SELECT, 
-    raw: true
-    });
-  
-  return results;
+  return result;
 };
 
 const findAllApplicantStatusByValue = async (recruitingId, value) => {
-  const query = `SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time
-                  FROM teams as t JOIN applicants as a 
-                      ON t.id = a.teams_id
-                  WHERE t.recruiting_id = :recruitingId
-                      AND a.name LIKE :value OR a.email LIKE :value
-                  GROUP BY a.id`;
-  const results = await db.sequelize.query(
-    query, 
-    {
-    replacements: {recruitingId: recruitingId, value: '%' + value + '%'},
-    type: db.Sequelize.QueryTypes.SELECT, 
-    raw: true
-    });
+  const query = 'SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time ' +
+    'FROM teams as t ' +
+    'JOIN applicants as a ON t.id = a.teams_id ' +
+    'WHERE t.recruiting_id = :recruitingId AND a.name LIKE :value OR a.email LIKE :value ' +
+    'GROUP BY a.id';
+  const options = {
+    replacements: {
+      recruitingId,
+      value: '%' + value + '%',
+    },
+    type: db.Sequelize.QueryTypes.SELECT,
+    raw: true,
+  };
+  const result = await db.sequelize.query(query, options);
   
-  return results;
+  return result;
 };
 
 const findAllApplicantStatusByTeams = async (recruitingId, teamsId) => {
-  const query = `SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time
-                  FROM teams as t JOIN applicants as a 
-                      ON t.id = a.teams_id
-                  WHERE t.recruiting_id = :recruitingId
-                        AND
-                        a.teams_id = :teamsId`;
-  
-  const results = await db.sequelize.query(
-    query, 
-    {
-    replacements: {recruitingId: recruitingId, teamsId: teamsId}, 
-    type: db.Sequelize.QueryTypes.SELECT, 
-    raw: true
-    });
-  
-  return results;
+  const query = 'SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time ' +
+    'FROM teams as t ' +
+    'JOIN applicants as a ON t.id = a.teams_id ' +
+    'WHERE t.recruiting_id = :recruitingId AND a.teams_id = :teamsId';
+  const options = {
+    replacements: {recruitingId, teamsId},
+    type: db.Sequelize.QueryTypes.SELECT,
+    raw: true,
+  };
+  const result = await db.sequelize.query(query, options);
+
+  return result;
 };
 
 const findAllApplicantStatusByStatus = async (recruitingId, applicationStatus) => {
-  const query = `SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time
-                  FROM teams as t JOIN applicants as a 
-                      ON t.id = a.teams_id
-                  WHERE t.recruiting_id = :recruitingId
-                        AND
-                        a.application_status = :applicationStatus`;
-  
-  const results = await db.sequelize.query(
-    query, 
-    {
-    replacements: {recruitingId: recruitingId, applicationStatus: applicationStatus}, 
-    type: db.Sequelize.QueryTypes.SELECT, 
-    raw: true
-    });
-  
-  return results;
+  const query = 'SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time' +
+    'FROM teams as t ' +
+    'JOIN applicants as a ON t.id = a.teams_id ' +
+    'WHERE t.recruiting_id = :recruitingId AND a.application_status = :applicationStatus';
+  const options = {
+    replacements: {recruitingId, applicationStatus},
+    type: db.Sequelize.QueryTypes.SELECT,
+    raw: true,
+  };
+  const result = await db.sequelize.query(query, options);
+
+  return result;
 };
 
 module.exports = {
--- src/app/applicant/infrastructure/resume_repository.js
+++ src/app/applicant/infrastructure/resume_repository.js
@@ -1,52 +1,37 @@
 const db = require('../../../common/model/sequelize.js');
 
 const findOneResume = async applicantId => {
-  const query = `SELECT a.id, t.name as teams_name, a.name, 
-                  a.email, a.phone, a.application_status, a.application_time,
-                  q.content as question, q.number, n.content as answer
-                  FROM 
-                    teams as t JOIN applicants as a 
-                        ON t.id = a.teams_id
-                    JOIN questions as q 
-                        ON t.id = q.teams_id
-                    JOIN answers as n
-                        ON q.id = n.questions_id AND a.id = n.applicants_id
-                  WHERE a.id = :applicantId`;
+  const query = 'SELECT a.id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time, q.content as question, q.number, n.content as answer ' +
+    'FROM teams as t ' +
+    'JOIN applicants as a ON t.id = a.teams_id ' +
+    'JOIN questions as q ON t.id = q.teams_id ' +
+    'JOIN answers as n ON q.id = n.questions_id AND a.id = n.applicants_id ' +
+    'WHERE a.id = :applicantId';
+  const options = {
+    replacements: {applicantId},
+    type: db.Sequelize.QueryTypes.SELECT,
+    raw: true,
+  };
+  const result = await db.sequelize.query(query, options);
   
-  const results = await db.sequelize.query(
-    query, 
-    {
-    replacements: {applicantId: applicantId}, 
-    type: db.Sequelize.QueryTypes.SELECT, 
-    raw: true
-    });
-  
-  return results;
+  return result;
 };
 
 const findOneApplicant = async applicantId => {
-  const query = `SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, 
-                  a.email, a.phone, a.application_status, a.application_time,
-                  q.content as question, q.number, n.content as answer
-                  FROM 
-                    teams as t JOIN applicants as a 
-                        ON t.id = a.teams_id
-                    JOIN questions as q 
-                        ON t.id = q.teams_id
-                    JOIN answers as n
-                        ON q.id = n.questions_id AND a.id = n.applicants_id
-                  WHERE a.id = :applicantId`;
+  const query = 'SELECT a.id, t.id as teams_id, t.name as teams_name, a.name, a.email, a.phone, a.application_status, a.application_time, q.content as question, q.number, n.content as answer ' +
+    'FROM teams as t ' +
+    'JOIN applicants as a ON t.id = a.teams_id ' +
+    'JOIN questions as q ON t.id = q.teams_id ' +
+    'JOIN answers as n ON q.id = n.questions_id AND a.id = n.applicants_id ' +
+    'WHERE a.id = :applicantId';
+  const options = {
+    replacements: {applicantId},
+    type: db.Sequelize.QueryTypes.SELECT,
+    raw: true,
+  };
+  const result = await db.sequelize.query(query, options);
 
-  const results = await db.sequelize.query(
-    query, {
-      replacements: {
-        applicantId: applicantId
-      },
-      type: db.Sequelize.QueryTypes.SELECT,
-      raw: true
-    });
-
-  return results;
+  return result;
 };
 
 module.exports = {
