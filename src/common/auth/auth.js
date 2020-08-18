7e2136aa792bb5519e3ee2d6be1aa85f28a632b3
forest.grass <korea8378@naver.com>
2020-08-18 12:08:19 +0900

    [ADD] Authorization Header의 Value(JWT 타입 추가- Bearer) 변경 (#38)
    * [ADD] Authorization Header의 Value(JWT 타입 추가- Bearer) 변경
    
    * [FIX] 오타 수정
    
    * [DELETE] 중복 되는 Toke 검증 로직 삭제
    
    * [FIX] 매직 스트링 상수로 변경
    
    * [FIX] JWT 위치 및 Path 변경

--- src/common/auth/auth.js
+++ src/common/auth/auth.js
@@ -1,22 +1,18 @@
-const Jwt = require('../../util/jwt.js');
+const Jwt = require('./jwt.js');
 const JwtConfig = require('../../../config/jwt-config.json');
 
 const authMiddleware = async (req, res, next) => {
-  // const token = req.headers.authorization;
-  const token = JwtConfig.token;
-
-  if(!token) {
-      return res.status(403).json({
-          success: false,
-          message: 'No Atuthentification - Check Token'
-      })
+  const token = req.headers.authorization;
+  try {
+    const decoded = await Jwt.verify(token);
+    req.decoded = decoded;
+    next(); 
+  } catch (err) {
+    return  res.status(403).json({
+      success: false,
+      message: 'No Authentication - Check Token'
+    });
   }
-
-  const decoded = await Jwt.verify(token);
-
-  req.decoded = decoded;
-
-  next();
 };
 
 module.exports = authMiddleware;
--- src/common/auth/jwt.js
+++ src/common/auth/jwt.js
@@ -1,17 +1,21 @@
 const Jwt = require('jsonwebtoken');
 const Util = require('util');
-const JwtConfig = require('../../config/jwt-config.json');
+const JwtConfig = require('../../../config/jwt-config.json');
 
 const jwtSign = Util.promisify(Jwt.sign);
 const jwtVerify = Util.promisify(Jwt.verify);
+const BEARER_INDEX = 7;
+const BEARER = 'Bearer ';
+const ONE_YEAR = '365d';
+const ISSUER = 'mash-up.com';
+const SBUJECT = 'userInfo';
 
 const sign = async (id, userId, role) => {
   try {
     const userInfo = {id: id, user_id: userId, role: role};
     const secretKey = JwtConfig.secret;
-    const options = {expiresIn: '365d', issuer: 'mash-up.com', subject: 'userInfo'};
+    const options = {expiresIn: ONE_YEAR, issuer: ISSUER, subject: SBUJECT};
     const result = await jwtSign(userInfo, secretKey, options);
-
     return result;
   } catch (err) {
     const error = new Error('Fail Auth - Cannot make Token');
@@ -22,13 +26,21 @@ const sign = async (id, userId, role) => {
 
 const verify = async token => {
   try {
-    return await jwtVerify(token, JwtConfig.secret);
+    if(!(await isBearer(token))) {
+      throw new Error();
+    } 
+    const typeCutToken = token.substring(BEARER_INDEX);
+    return await jwtVerify(typeCutToken, JwtConfig.secret);
   } catch (err) {
     const error = new Error('Fail Auth - Cannot Authenticate');
     error.status = 400;
     throw error;
   }
 };
+
+const isBearer = async token => {
+  return token.startsWith(BEARER);
+}
 
 module.exports = {
   sign,
