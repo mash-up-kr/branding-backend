48b0cb5c4a31518137b8d60bcb066959f27cb163
csh <csh@comcom.ai>
2020-08-22 19:04:50 +0900

    Add cors module

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
-
 const app = express();
-
+const cors = require('cors');
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  // res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
-  next();
-});
+app.use(cors());
 
 app.use('/api', require('./app/'));
 
