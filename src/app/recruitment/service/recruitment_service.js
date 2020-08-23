f3908a08a165cd0425f5290c5d405ec40f7cc102
korea8378 <korea8378@naver.com>
2020-08-24 02:23:43 +0900

    [MODIFY] Response 변경

--- src/app/recruitment/service/recruitment_service.js
+++ src/app/recruitment/service/recruitment_service.js
@@ -18,16 +18,14 @@ const getRecruitment = async (role) => {
     banner: latelyRecruiting.main_banner,
     title: latelyRecruiting.title,
     introduction: latelyRecruiting.introduction,
-    schedule: {
-      recruitment_start: latelyRecruiting.recruitment_start_period,
-      recruitment_end: latelyRecruiting.recruitment_end_period,
-      document_acceptance_start: latelyRecruiting.document_acceptance_start_period,
-      document_acceptance_end: latelyRecruiting.document_acceptance_end_period,
-      interview_start: latelyRecruiting.interview_start_period,
-      interview_end: latelyRecruiting.interview_end_period,
-      final_acceptance_start: latelyRecruiting.final_acceptance_start_period,
-      final_acceptance_end: latelyRecruiting.final_acceptance_end_period
-    }
+    recruitment_start: latelyRecruiting.recruitment_start_period,
+    recruitment_end: latelyRecruiting.recruitment_end_period,
+    document_acceptance_start: latelyRecruiting.document_acceptance_start_period,
+    document_acceptance_end: latelyRecruiting.document_acceptance_end_period,
+    interview_start: latelyRecruiting.interview_start_period,
+    interview_end: latelyRecruiting.interview_end_period,
+    final_acceptance_start: latelyRecruiting.final_acceptance_start_period,
+    final_acceptance_end: latelyRecruiting.final_acceptance_end_period
   };
 };
 
