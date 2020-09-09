0cbb706f3a106e6c16408cbddcaae203f33cff41
csh <csh@comcom.ai>
2020-09-09 22:57:16 +0900

    Fix crash

--- src/app/recruitment/service/team_service.js
+++ src/app/recruitment/service/team_service.js
@@ -2,34 +2,34 @@ const Team = require('../domain/team.js');
 const timeCoverter = require('../../../util/time_coverter.js');
 
 const getTeams = async (recruitmentId) => {
-  const teams = await Team.findAll({ where: { recruitment_id: recruitmentId}});
+  const teams = await Team.findAll({where: {recruitment_id: recruitmentId}});
 
   const results = [];
-  for(let i = 0; i < teams.length; i++) {
+  for (let i = 0; i < teams.length; i++) {
     const updateTime = timeCoverter.toTimestamp(teams[i].update_time);
     const result = {
-      id : teams[i].id,
-      name : teams[i].name,
-      update_time : updateTime,
-    }
+      id: teams[i].id,
+      name: teams[i].name,
+      update_time: updateTime,
+    };
     results.push(result);
   }
 
   return results;
-}
+};
 
 const getTeam = async (recruitmentId, teamId) => {
-  const team = await Team.findOne({ where: { recruitment_id: recruitmentId, id: teamId }});
+  const team = await Team.findOne({where: {recruitment_id: recruitmentId, id: teamId}});
 
-  return  {
+  return {
     id: team.id,
     name: team.name,
     recruitment_id: team.recruitment_id,
     resume_link: team.resume_link,
     sheets_link: team.sheets_link,
-    contents: team.introduction 
+    contents: team.introduction,
   };
-}
+};
 
 const insertTeam = async (recruitmentId, team) => {
   const result = await Team.create({
@@ -37,27 +37,27 @@ const insertTeam = async (recruitmentId, team) => {
     recruitment_id: recruitmentId,
     resume_link: team.resume_link,
     sheets_link: team.sheets_link,
-    introduction: team.contents
+    introduction: team.contents,
   });
 
-  return  {
+  return {
     id: result.id,
     name: result.name,
     recruitment_id: result.recruitment_id,
     resume_link: result.resume_link,
     sheets_link: result.sheets_link,
-    contents: result.introduction 
+    contents: result.introduction,
   };
 };
 
 const deleteTeam = async (recruitmentId, teamId) => {
   const result = await Team.destroy({
     where: {
-        id: teamId, recruitment_id: recruitmentId
-    }
+      id: teamId, recruitment_id: recruitmentId,
+    },
   });
 
-  if(!result) {
+  if (!result) {
     const error = new Error('Not Found Recruitment or Team');
     error.status = 404;
     throw error;
@@ -67,23 +67,25 @@ const deleteTeam = async (recruitmentId, teamId) => {
 };
 
 const updateTeam = async (recruitmentId, team, teamId) => {
-  const result = await Team.findOne({ where: { recruitment_id: recruitmentId, id: teamId }});
+  const result = await Team.findOne({where: {recruitment_id: recruitmentId, id: teamId}});
 
   result.changeInfo(team.name, team.resume_link, team.sheets_link, team.contents);
 
-  await result.update({ name: result.name,
+  await result.update({
+    name: result.name,
     resume_link: result.resume_link,
     sheets_link: result.sheets_link,
     introduction: result.introduction,
-    update_time: Date.now() });
+    update_time: Date.now(),
+  });
 
-  return  {
+  return {
     id: result.id,
     name: result.name,
     recruitment_id: result.recruitment_id,
     resume_link: result.resume_link,
     sheets_link: result.sheets_link,
-    contents: result.introduction 
+    contents: result.introduction,
   };
 };
 
